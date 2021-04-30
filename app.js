22550852bd411ffbd58305c8d425e480f137a485
k_pilat <k_pilat@utb.cz>
2021-04-30 19:26:57 +0200

    Easy authentication works

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 const paramModify = require(__dirname + "/scripts/paramModify");
 const queries = require(__dirname + "/scripts/queries")
@@ -14,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-const mongooseConnectOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+const mongooseConnectOptions = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true};
 const connection = mongoose.connect(process.env.MONGO_URL, mongooseConnectOptions);
 
 // Variables
@@ -33,11 +35,34 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24
-    }
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 
 
@@ -68,12 +93,56 @@ app.get('/', function(req, res) {
 
 });
 
-// app.get('/:recover', function(req, res) {
-//     if(req.params.recover === "1"){
-//         req.session.recover = req.params.recover;
-//         res.redirect("/");
-//     }
-// });
+app.get('/login', function(req, res) {
+    res.render("login");
+});
+
+app.get('/register', function(req, res) {
+    res.render("register");
+});
+
+app.get('/new-ad', function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("new-ad");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+app.post('/register', function(req, res){
+
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            })
+        }
+    })
+
+});
+
+app.post("/login", function(req, res){
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            })
+        }
+    })
+});
 
 app.get('/results', function(req, res) {
 
